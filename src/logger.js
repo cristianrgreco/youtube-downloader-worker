873976d5f4d2de6eded3b267f812b60b9ff96437
Cristian Greco <cristianrgreco@gmail.com>
2018-02-20 21:22:54 +0000

    Change log format

--- src/logger.js
+++ src/logger.js
@@ -4,7 +4,7 @@ const {logLevel} = require('./conf')
 const logger = winston.createLogger({
   level: logLevel,
   transports: [
-    new winston.transports.Console(),
+    new winston.transports.Console({ format: winston.format.simple() }),
     new winston.transports.File({ filename: 'combined.log' })
   ]
 })
